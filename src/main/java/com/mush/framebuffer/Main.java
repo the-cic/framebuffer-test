1fca89a83baf39cca7332ff51c61734c6d0d12b6
cic <cic@dhimahi.com>
2018-04-12 11:03:52 +0200

    ~ tring with RandomAccessFile

--- src/main/java/com/mush/framebuffer/Main.java
+++ src/main/java/com/mush/framebuffer/Main.java
@@ -8,6 +8,7 @@ package com.mush.framebuffer;
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,78 +19,76 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("preparing...");
+        System.out.println("preparing..." + args.length);
 
+//        String fbFile = "/dev/fb0";
+        String fbFile = args.length == 0 ? "test.out" : args[0];
         int width = 1824;
         int height = 984;
 
-        byte[] buffer = new byte[width * height * 4];
-        
+        byte[] buffer1 = new byte[width * height * 4];
+        byte[] buffer2 = new byte[width * height * 4];
+
         long time0 = System.currentTimeMillis();
-        
-        for (int i = 0; i < buffer.length; i++) {
-            buffer[i] = i % 4 == 0 ? (byte)0xFF : 0x30;
+
+        for (int i = 0; i < buffer1.length; i++) {
+            buffer1[i] = i % 4 == 0 ? (byte) 0xFF : 0x30;
+            buffer2[i] = i % 4 == 1 ? (byte) 0xFF : 0x30;
         }
-        
-//        byte[] bytesLine = new byte[width];
-//        for (int i = 0; i < width / 4; i++) {
-//            bytesLine[i * 4 + 0] = (byte) 0xff;
-//            bytesLine[i * 4 + 1] = 0x00;
-//            bytesLine[i * 4 + 2] = 0x00;
-//            bytesLine[i * 4 + 3] = 0x00;
-//        }
 
         long time1 = System.currentTimeMillis();
 
-        try (FileOutputStream fos = new FileOutputStream("/dev/fb0");
+        useStream(fbFile, buffer1);
+
+        long time2 = System.currentTimeMillis();
+
+        useFile(fbFile, buffer1);
+
+        long time3 = System.currentTimeMillis();
+
+        int count = useFileRefresh(fbFile, buffer1, buffer2);
+
+        long time4 = System.currentTimeMillis();
+
+        System.out.println("drawing          : " + (time1 - time0) + " ms");
+        System.out.println("writing stream   : " + (time2 - time1) + " ms");
+        System.out.println("writing file     : " + (time3 - time2) + " ms");
+        System.out.println("writing file " + count + "x : " + (time4 - time3) + " ms " + ((time4 - time3) / count) + " ms/f");
+    }
+
+    private static void useStream(String filename, byte[] buffer) {
+        try (FileOutputStream fos = new FileOutputStream(filename);
                 BufferedOutputStream bos = new BufferedOutputStream(fos)) {
-
-//            byte[] bytesA = new byte[]{(byte) 0xFF, 0x00, 0x00, 0x00};
-//            byte[] bytesB = new byte[]{0x00, (byte) 0xFF, 0x00, 0x00};
-//            byte[] bytesC = new byte[]{0x00, 0x00, (byte) 0xFF, 0x00};
-//            byte[] bytesD = new byte[]{0x00, 0x00, 0x00, (byte) 0xFF};
-//
-//            int len1 = (height / 4) /2;
-//            
-//            for (int j = 0; j < len1; j++) {
-//
-//                for (int i = 0; i < width; i++) {
-//                    bos.write(bytesA);
-//                }
-//
-//                for (int i = 0; i < width; i++) {
-//                    bos.write(bytesB);
-//                }
-//
-//                for (int i = 0; i < width; i++) {
-//                    bos.write(bytesC);
-//                }
-//
-//                for (int i = 0; i < width; i++) {
-//                    bos.write(bytesD);
-//                }
-//            }
-//            
-//            int len2 = (height ) - 50;
-//            
-//            for (int j = 0; j < len2; j++) {
-//                bos.write(bytesLine);
-//            }
             bos.write(buffer);
-            
-
-
-//            bos.flush();
-//            fos.flush();
 
         } catch (IOException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        long time2 = System.currentTimeMillis();
-        
-        System.out.println("drawing: " + (time1 - time0) + " ms");
-        System.out.println("writing: " + (time2 - time1) + " ms");
+    }
+
+    private static void useFile(String filename, byte[] buffer) {
+        try (RandomAccessFile raf = new RandomAccessFile(filename, "rw")) {
+            raf.write(buffer);
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private static int useFileRefresh(String filename, byte[] buffer1, byte[] buffer2) {
+        int c = 0;
+        try (RandomAccessFile raf = new RandomAccessFile(filename, "rw")) {
+            for (int i = 0; i < 10; i++) {
+                raf.seek(0);
+                raf.write(buffer1);
+                c++;
+                raf.seek(0);
+                raf.write(buffer2);
+                c++;
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return c;
     }
 
 }
