c083b66fb22225746d334957890e598272ca4046
the-cic <the-cic@github.com>
2018-04-12 20:39:45 +0200

    ~ more timing

--- src/main/java/com/mush/framebuffer/Main.java
+++ src/main/java/com/mush/framebuffer/Main.java
@@ -9,6 +9,7 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Transparency;
 import java.awt.color.ColorSpace;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.ComponentColorModel;
@@ -30,67 +31,101 @@ import java.util.logging.Logger;
  */
 public class Main {
 
+    byte[] buffer1;
+    byte[] buffer2;
+    StringBuilder sb;
+    long[] lastTime = null;
+    Graphics2D g;
+
     public static void main(String[] args) {
-        System.out.println("preparing..." + args.length);
+        new Main(args);
+    }
+
+    private long tick(int level) {
+        if (lastTime == null) {
+            lastTime = new long[5];
+            for (int i = 0; i < lastTime.length; i++) {
+                lastTime[i] = 0;
+            }
+        }
+        long tock = System.currentTimeMillis();
+        long tick = tock - lastTime[level];
+        lastTime[level] = tock;
+        return tick;
+    }
+
+    private Main(String[] args) {
+        System.out.println("preparing...");
 
 //        String fbFile = "/dev/fb0";
         String fbFile = args.length == 0 ? "test.out" : args[0];
+        System.out.println("Filename: " + fbFile);
+
+        sb = new StringBuilder();
+        tick(0);
+
         int width = 1824;
         int height = 984;
 
-        byte[] buffer1 = new byte[width * height * 4];
-//        byte[] buffer2 = new byte[width * height * 4];
+        buffer1 = new byte[width * height * 4];
 
-        long time00 = System.currentTimeMillis();
-
-        BufferedImage image = createImage(width, height);
-        byte[] buffer2 = getImageByteBuffer(image);
-
-        long time0 = System.currentTimeMillis();
+        sb.append("Creating buffer       : ").append(tick(0)).append("ms\n");
 
         for (int i = 0; i < buffer1.length; i++) {
             buffer1[i] = i % 4 == 0 ? (byte) 0xFF : 0x30;
-            //buffer2[i] = i % 4 == 1 ? (byte) 0xFF : 0x30;
         }
 
-        long time1 = System.currentTimeMillis();
+        sb.append("Drawing loop to buffer: ").append(tick(0)).append("ms\n");
+
+        BufferedImage image = createImage(width, height);
+//        buffer2 = getImageByteBuffer(image);
+
+        sb.append("Creating image        : ").append(tick(0)).append("ms\n");
 
         useStream(fbFile, buffer1);
-
-        long time2 = System.currentTimeMillis();
+        
+        sb.append("Writing to stream     : ").append(tick(0)).append("ms\n");
 
         useFile(fbFile, buffer1);
+        
+        sb.append("Writing to file       : ").append(tick(0)).append("ms\n");
 
-        long time3 = System.currentTimeMillis();
-
-        int count = useFileRefresh(fbFile, buffer1, buffer2);
-
-        long time4 = System.currentTimeMillis();
-
-        System.out.println("drawing image    : " + (time0 - time00) + " ms");
-        System.out.println("drawing loop     : " + (time1 - time0) + " ms");
-        System.out.println("writing stream   : " + (time2 - time1) + " ms");
-        System.out.println("writing file     : " + (time3 - time2) + " ms");
-        System.out.println("writing file " + count + "x : " + (time4 - time3) + " ms " + ((time4 - time3) / count) + " ms/f");
+        int count = useFileRefresh(fbFile/*, buffer1, buffer2*/);
+        
+        long duration = tick(0);
+        sb.append("Writing to file ").append(count).append("x : ").append(duration).append("ms ").append(duration / count).append(" ms/f\n");
+        
+        System.out.println(sb.toString());
     }
 
-    private static BufferedImage createImage(int width, int height) {
+    private BufferedImage createImage(int width, int height) {
+        tick(1);
         int samplesPerPixel = 4;
         int[] bandOffsets = {2, 1, 0, 3}; // BGRA order
 
         byte[] bgraPixelData = new byte[width * height * samplesPerPixel];
+        
+        sb.append(" Creating img buffer  : ").append(tick(1)).append("ms\n");
 
         DataBuffer buffer = new DataBufferByte(bgraPixelData, bgraPixelData.length);
+        sb.append(" DataBuffer           : ").append(tick(1)).append("ms\n");
         WritableRaster raster = Raster.createInterleavedRaster(buffer, width, height, samplesPerPixel * width, samplesPerPixel, bandOffsets, null);
+        sb.append(" Raster               : ").append(tick(1)).append("ms\n");
 
         ColorModel colorModel = new ComponentColorModel(ColorSpace.getInstance(ColorSpace.CS_sRGB), true, false, Transparency.TRANSLUCENT, DataBuffer.TYPE_BYTE);
 
         BufferedImage image = new BufferedImage(colorModel, raster, colorModel.isAlphaPremultiplied(), null);
+        
+        sb.append(" BufferedImage        : ").append(tick(1)).append("ms\n");
 
-        Graphics2D g = image.createGraphics();
+        g = image.createGraphics();
+        
+        sb.append(" image.createGraphics : ").append(tick(1)).append("ms\n");
 
         g.setBackground(Color.GRAY);
-        g.clearRect(0, 0, width/2, height);
+        g.clearRect(0, 0, width / 2, height);
+        
+        sb.append(" g.clearRect          : ").append(tick(1)).append("ms\n");
 
         g.setColor(Color.RED);
         g.drawRect(100, 100, 100, 100);
@@ -100,11 +135,15 @@ public class Main {
 
         g.setColor(Color.BLUE);
         g.drawRect(170, 150, 100, 100);
+        
+        sb.append(" g.drawRect 3x        : ").append(tick(1)).append("ms\n");
+
+        buffer2 = bgraPixelData;
 
         return image;
     }
 
-    private static byte[] getImageByteBuffer(BufferedImage image) {
+    private byte[] getImageByteBuffer(BufferedImage image) {
 //        ByteArrayOutputStream baos = new ByteArrayOutputStream();
 //        ImageIO.write(originalImage, "jpg", baos);
 //        baos.flush();
@@ -115,7 +154,7 @@ public class Main {
         return ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
     }
 
-    private static void useStream(String filename, byte[] buffer) {
+    private void useStream(String filename, byte[] buffer) {
         try (FileOutputStream fos = new FileOutputStream(filename);
                 BufferedOutputStream bos = new BufferedOutputStream(fos)) {
             bos.write(buffer);
@@ -125,7 +164,7 @@ public class Main {
         }
     }
 
-    private static void useFile(String filename, byte[] buffer) {
+    private void useFile(String filename, byte[] buffer) {
         try (RandomAccessFile raf = new RandomAccessFile(filename, "rw")) {
             raf.write(buffer);
         } catch (IOException ex) {
@@ -133,13 +172,25 @@ public class Main {
         }
     }
 
-    private static int useFileRefresh(String filename, byte[] buffer1, byte[] buffer2) {
+    private int useFileRefresh(String filename/*, byte[] buffer1, byte[] buffer2*/) {
         int c = 0;
+        Color[] colors = new Color[]{Color.RED, Color.GREEN, Color.YELLOW, Color.BLUE, Color.ORANGE, Color.MAGENTA};
+        
+        AffineTransform t = g.getTransform();
+        g.scale(2, 2);
+        
         try (RandomAccessFile raf = new RandomAccessFile(filename, "rw")) {
-            for (int i = 0; i < 10; i++) {
-                raf.seek(0);
-                raf.write(buffer1);
-                c++;
+            for (int i = 0; i < 100; i++) {
+//                raf.seek(0);
+//                raf.write(buffer1);
+//                c++;
+                int x = 200 + (int)(Math.random() * 500);
+                int y = 100 + (int)(Math.random() * 200);
+                g.setColor(colors[c % colors.length]);
+                g.fillRect(x, y, 100, 100);
+                g.setColor(Color.WHITE);
+                g.drawRect(x, y, 100, 100);
+                
                 raf.seek(0);
                 raf.write(buffer2);
                 c++;
@@ -147,6 +198,9 @@ public class Main {
         } catch (IOException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
+        g.setTransform(t);
+        
         return c;
     }
 
